c0c98fba0ab245b7e0aac16c5d42b146a1b67cb0
Pavel <PKalennik@alfabank.ru>
2023-12-16 17:01:32 +0300

    another start setup

---
+++ NewTechApplication/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NewTechApplication"
+include(":app")
+include(":main_screen")
+include(":splash_screen")
